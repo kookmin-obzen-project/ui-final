f9fbc91df65dad1876776006d5703777790659e9
wjdtnghks123 <wjdtnghks123@nate.com>
2023-09-21 20:19:51 +0900

    Feat: Add user_session api

--- server/db/model_session.py
+++ server/db/model_session.py
@@ -1,31 +1,32 @@
-# import sys
-# sys.path.append("update-chat-server/server/")
-# from sqlalchemy import *
-# from fastapi import FastAPI, HTTPException, Depends, status, APIRouter
-# from pydantic import *
-# from typing import Annotated
-# from db.db import *
-# from sqlalchemy.orm import Session
-# from fastapi.middleware.cors import CORSMiddleware
-# from datetime import datetime
+import sys
+sys.path.append("update-chat-server/server/")
+from sqlalchemy import *
+from fastapi import FastAPI, HTTPException, Depends, status, APIRouter
+from pydantic import *
+from typing import Annotated
+from db.db import *
+from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
 
-# class Session(Base):
-#     __tablename__ = 'session'
+class Session(Base):
+    __tablename__ = 'session'
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     created_at = Column(DateTime, default=func.now())
-#     user_session = Column(String, unique=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=func.now())
+    expired_at = Column(DateTime, default= None)
+    user_session = Column(String, unique=True, index=True)
 
-# class SessionBase(BaseModel):
-#     created_at : datetime = datetime.now()
-#     user_session : str
+class SessionBase(BaseModel):
+    created_at : datetime = datetime.now()
+    user_session : str
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-# db_dependency = Annotated[Session, Depends(get_db)]
+db_dependency = Annotated[Session, Depends(get_db)]
 
--- server/routes/session.py
+++ server/routes/session.py
@@ -4,18 +4,27 @@ sys.path.append("update-chat-server/server")
 from fastapi import FastAPI, HTTPException, Depends, status, APIRouter, Request
 from pydantic import *
 from db.model_session  import *
+import uuid
 
 app = FastAPI()
 router = APIRouter()
 
-### 여기서부터~~~ 고쳐봅시다 session 파트
-# @router.post("/", status_code=status.HTTP_201_CREATED)
-# async def create_session( db: db_dependency):
-#     db_chatRoom = Session()
-#     db.add(db_chatRoom)
-#     db.commit()
+### 여기서부터 고쳐봅시다 session 파트
+@router.get("/", status_code=status.HTTP_201_CREATED)
+async def create_session( db: db_dependency):
+    session_expiration_time = 3600 # 초 단위, 3600 = 1시간, record expiration time = cookie expiration time
     
+    user_session = str(uuid.uuid4())
+    response = JSONResponse(content={"message": "Session ID set successfully"})
+    response.set_cookie(key="session_id", value=user_session, max_age=session_expiration_time)
     
+    db_session = Session()
+    db_session.user_session = user_session
+    db_session.expired_at = datetime.now() + timedelta(seconds=session_expiration_time)
+    db.add(db_session)
+    db.commit()
+    
+    return response
 #     def create_session(user_id: int, db: Session = Depends(SessionLocal)):
 #     session_id = str(uuid4())
 #     db_session = SessionModel(session_id=session_id, user_id=user_id)
