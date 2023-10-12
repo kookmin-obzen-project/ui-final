c202ed4ab517c6654edb3b3d475b4ab27672fe0a
kookso <117020362+kookso@users.noreply.github.com>
2023-10-12 22:47:41 +0900

    Update Header.tsx
    modify the clock function.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,35 @@
-export default function Header() {
+import React, { useState, useEffect } from 'react';
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+  const [currentTime, setCurrentTime] = useState<string>(
+    formatTime(new Date())
+  );
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(formatTime(new Date()));
+    }, 1000); 
+
+    return () => clearInterval(intervalId);
+  }, []); 
+
+  function formatTime(date: Date): string {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const period = hours >= 12 ? '오후' : '오전';
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+
+    return `${period} ${formattedHours}:${formattedMinutes}`;
+  }
+
   return (
     <div>
       <div className="h-20 bg-white pb-4">
-        <div className="flex h-5 items-center p-10 justify-between"> {/* Use justify-between class */}
+        <div className="flex h-5 items-center p-10 justify-between">
           <div className="flex lg:flex-1">
             <a href="#!" className="-m-1.5 p-1.5">
               <img
@@ -13,12 +40,13 @@ export default function Header() {
               <span className="sr-only">Obzen</span>
             </a>
           </div>
-          <div className="text-2xl font-bold">
-            {new Date().toLocaleTimeString()}
-          </div>
+          <div className="text-2xl font-bold">{currentTime}</div>
         </div>
       </div>
       <div className="border-b border-default-border"></div>
     </div>
   );
-}
+};
+
+export default Header;
+
