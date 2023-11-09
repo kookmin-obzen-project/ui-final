f709a634fabba01b6507328ccf88711a09f6eaa3
YunHaaaa <qlxqlrt2012@gmail.com>
2023-11-09 22:49:37 +0900

    Feat: add customer

--- server/db/data/customer.py
+++ server/db/data/customer.py
@@ -1,16 +1,15 @@
 
 from faker import Faker
 import random
+import pymysql
 
 # Faker 객체 생성
 fake = Faker()
 
-import pymysql
-
 # 데이터베이스 연결 정보
 host = 'localhost'
 port = 3306
-database = 'obzen'
+database = 'obzen_test'
 username = 'root'
 password = 'password'
 
@@ -20,22 +19,15 @@ conn = pymysql.connect(host=host, port=port, database=database, user=username, p
 # 커서 생성
 cursor = conn.cursor()
 
-
 # CHANNEL 테이블에서 STORE_NO와 STORE_NM을 가져와서 channel_data 리스트 생성
 cursor.execute("SELECT STORE_NO, STORE_NM FROM CHANNEL")
 rows = cursor.fetchall()
 channel_data = [(store_no, store_nm) for store_no, store_nm in rows]
 
-# 데이터베이스 연결 종료
-conn.close()
-
-# channel_data 리스트 출력 (예제로 출력함)
-# for data in channel_data:
-#     print(data)
 
 
-# 30개의 가짜 데이터 생성
-for _ in range(30):
+# 가짜 데이터 생성
+def get_data():
     cust_no = fake.unique.random_int(min=199220000000, max=202329999999)  # 고객 번호 (무작위 범위 설정)
     age = random.choice(['10대 이하', '20대', '30대', '40대', '50대', '60대', '70대', '80대 이상'])  # 나이
     frgn_cd_nm = random.choice(['내국인', '외국인'])  # 내외국인
@@ -59,5 +51,73 @@ for _ in range(30):
     point_exp = fake.random_int(min=0, max=1000)  # 포인트 만료 (무작위 범위 설정)
     point_bfh = fake.random_int(min=0, max=500)  # 포인트 사용 (무작위 범위 설정)
 
-    # 생성된 가짜 데이터 출력 또는 데이터베이스에 삽입할 수 있습니다.
-    print(f"('{cust_no}', '{age}', '{frgn_cd_nm}', {mng_store_cd}, '{mng_store_cd_nm}', '{channel_second}', '{gndr_cd_nm}', '{birth_year}', '{birth_date}', '{tm_yn}', '{sms_yn}', '{join_date}', '{withdraw_cd_nm}', '{cust_seg_cd_nm}', '{cust_grd_cd_nm}', '{prmtd_date}', '{cust_grd_cd_nm_last}', '{subsc_path_cd_nm}', {point_bal}, {point_exp}, {point_bfh}),")
+    return cust_no, age, frgn_cd_nm, mng_store_cd, mng_store_cd_nm, channel_second, gndr_cd_nm, birth_year, birth_date, tm_yn, sms_yn, join_date, withdraw_cd_nm, cust_seg_cd_nm, cust_grd_cd_nm, prmtd_date, cust_grd_cd_nm_last, subsc_path_cd_nm, point_bal, point_exp, point_bfh
+
+
+# 데이터 삽입 쿼리
+insert_query = """
+INSERT INTO customer (
+    cust_no,
+    age,
+    frgn_cd_nm,
+    mng_store_cd,
+    mng_store_cd_nm,
+    channel_second,
+    gndr_cd_nm,
+    birth_year,
+    birth_date,
+    tm_yn,
+    sms_yn,
+    join_date,
+    withdraw_cd_nm,
+    cust_seg_cd_nm,
+    cust_grd_cd_nm,
+    prmtd_date,
+    cust_grd_cd_nm_last,
+    subsc_path_cd_nm,
+    point_bal,
+    point_exp,
+    point_bfh
+) VALUES (
+    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+)
+"""
+
+# 데이터 삽입
+for _ in range(100):
+    # 데이터를 가져옵니다.
+    cust_no, age, frgn_cd_nm, mng_store_cd, mng_store_cd_nm, channel_second, gndr_cd_nm, birth_year, birth_date, tm_yn, sms_yn, join_date, withdraw_cd_nm, cust_seg_cd_nm, cust_grd_cd_nm, prmtd_date, cust_grd_cd_nm_last, subsc_path_cd_nm, point_bal, point_exp, point_bfh = get_data()
+
+    # 데이터를 쿼리에 바인딩합니다.
+    args = (
+        cust_no,
+        age,
+        frgn_cd_nm,
+        mng_store_cd,
+        mng_store_cd_nm,
+        channel_second,
+        gndr_cd_nm,
+        birth_year,
+        birth_date,
+        tm_yn,
+        sms_yn,
+        join_date,
+        withdraw_cd_nm,
+        cust_seg_cd_nm,
+        cust_grd_cd_nm,
+        prmtd_date,
+        cust_grd_cd_nm_last,
+        subsc_path_cd_nm,
+        point_bal,
+        point_exp,
+        point_bfh,
+    )
+
+    # 데이터를 삽입합니다.
+    cursor.execute(insert_query, args)
+
+# 데이터베이스 커밋
+conn.commit()
+
+# 데이터베이스 연결 종료
+conn.close()
